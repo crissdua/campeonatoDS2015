dbd451065a294633de4a8ececd814b7c9bf4a8eb
Dennys Iván <navi93@live.com>
2015-08-30 20:12:06 -0600

    Revert "Arreglo de nueva categoria"
    This reverts commit 722b518f968bfdc493d05fbb7ddd8a71a286709a.

--- campeonato/frmnuevacategoria.Designer.cs
+++ campeonato/frmnuevacategoria.Designer.cs
@@ -38,19 +38,12 @@
             this.txtObervaciones = new System.Windows.Forms.TextBox();
             this.btnRegreso = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
-            this.dgvCategoria = new System.Windows.Forms.DataGridView();
-            this.btnAgregarCategoria = new System.Windows.Forms.Button();
-            this.btnModificar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.lblCodigo = new System.Windows.Forms.Label();
-            this.btnModificar2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 315);
+            this.label1.Location = new System.Drawing.Point(50, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 0;
@@ -59,7 +52,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(86, 345);
+            this.label2.Location = new System.Drawing.Point(50, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 1;
@@ -68,7 +61,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(121, 382);
+            this.label3.Location = new System.Drawing.Point(52, 121);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 13);
             this.label3.TabIndex = 2;
@@ -77,7 +70,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(86, 431);
+            this.label4.Location = new System.Drawing.Point(48, 186);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 13);
             this.label4.TabIndex = 3;
@@ -85,9 +78,9 @@
             // 
             // txtNombreCampeonato
             // 
-            this.txtNombreCampeonato.Location = new System.Drawing.Point(169, 312);
+            this.txtNombreCampeonato.Location = new System.Drawing.Point(120, 36);
             this.txtNombreCampeonato.Name = "txtNombreCampeonato";
-            this.txtNombreCampeonato.Size = new System.Drawing.Size(268, 20);
+            this.txtNombreCampeonato.Size = new System.Drawing.Size(157, 20);
             this.txtNombreCampeonato.TabIndex = 4;
             // 
             // lstEdad
@@ -120,9 +113,9 @@
             "28",
             "29",
             "30"});
-            this.lstEdad.Location = new System.Drawing.Point(169, 338);
+            this.lstEdad.Location = new System.Drawing.Point(132, 69);
             this.lstEdad.Name = "lstEdad";
-            this.lstEdad.Size = new System.Drawing.Size(65, 30);
+            this.lstEdad.Size = new System.Drawing.Size(76, 30);
             this.lstEdad.TabIndex = 5;
             // 
             // cmbGenero
@@ -131,26 +124,26 @@
             this.cmbGenero.Items.AddRange(new object[] {
             "Masculino",
             "Femenino"});
-            this.cmbGenero.Location = new System.Drawing.Point(169, 374);
+            this.cmbGenero.Location = new System.Drawing.Point(120, 118);
             this.cmbGenero.Name = "cmbGenero";
-            this.cmbGenero.Size = new System.Drawing.Size(268, 21);
+            this.cmbGenero.Size = new System.Drawing.Size(157, 21);
             this.cmbGenero.TabIndex = 6;
             // 
             // txtObervaciones
             // 
-            this.txtObervaciones.Location = new System.Drawing.Point(169, 403);
+            this.txtObervaciones.Location = new System.Drawing.Point(132, 162);
             this.txtObervaciones.Multiline = true;
             this.txtObervaciones.Name = "txtObervaciones";
-            this.txtObervaciones.Size = new System.Drawing.Size(268, 71);
+            this.txtObervaciones.Size = new System.Drawing.Size(145, 65);
             this.txtObervaciones.TabIndex = 7;
             // 
             // btnRegreso
             // 
             this.btnRegreso.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnRegreso.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRegreso.Location = new System.Drawing.Point(716, 230);
+            this.btnRegreso.Location = new System.Drawing.Point(415, 138);
             this.btnRegreso.Name = "btnRegreso";
-            this.btnRegreso.Size = new System.Drawing.Size(110, 41);
+            this.btnRegreso.Size = new System.Drawing.Size(142, 44);
             this.btnRegreso.TabIndex = 8;
             this.btnRegreso.Text = "Regresar";
             this.btnRegreso.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -159,7 +152,7 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(473, 431);
+            this.btnGuardar.Location = new System.Drawing.Point(415, 69);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(142, 44);
             this.btnGuardar.TabIndex = 9;
@@ -167,79 +160,11 @@
             this.btnGuardar.UseVisualStyleBackColor = true;
             this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
-            // dgvCategoria
-            // 
-            this.dgvCategoria.AllowUserToAddRows = false;
-            this.dgvCategoria.AllowUserToDeleteRows = false;
-            this.dgvCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCategoria.Location = new System.Drawing.Point(6, 12);
-            this.dgvCategoria.Name = "dgvCategoria";
-            this.dgvCategoria.ReadOnly = true;
-            this.dgvCategoria.Size = new System.Drawing.Size(688, 269);
-            this.dgvCategoria.TabIndex = 10;
-            this.dgvCategoria.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCategoria_CellContentClick);
-            // 
-            // btnAgregarCategoria
-            // 
-            this.btnAgregarCategoria.Location = new System.Drawing.Point(716, 39);
-            this.btnAgregarCategoria.Name = "btnAgregarCategoria";
-            this.btnAgregarCategoria.Size = new System.Drawing.Size(110, 41);
-            this.btnAgregarCategoria.TabIndex = 11;
-            this.btnAgregarCategoria.Text = "Agregar Categoría";
-            this.btnAgregarCategoria.UseVisualStyleBackColor = true;
-            this.btnAgregarCategoria.Click += new System.EventHandler(this.btnAgregarCategoria_Click);
-            // 
-            // btnModificar
-            // 
-            this.btnModificar.Location = new System.Drawing.Point(473, 382);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(142, 44);
-            this.btnModificar.TabIndex = 12;
-            this.btnModificar.Text = "Modificar";
-            this.btnModificar.UseVisualStyleBackColor = true;
-            this.btnModificar.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Location = new System.Drawing.Point(716, 162);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(109, 41);
-            this.btnEliminar.TabIndex = 13;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
-            // 
-            // lblCodigo
-            // 
-            this.lblCodigo.AutoSize = true;
-            this.lblCodigo.Location = new System.Drawing.Point(26, 382);
-            this.lblCodigo.Name = "lblCodigo";
-            this.lblCodigo.Size = new System.Drawing.Size(35, 13);
-            this.lblCodigo.TabIndex = 14;
-            this.lblCodigo.Text = "label5";
-            // 
-            // btnModificar2
-            // 
-            this.btnModificar2.Location = new System.Drawing.Point(717, 98);
-            this.btnModificar2.Name = "btnModificar2";
-            this.btnModificar2.Size = new System.Drawing.Size(109, 41);
-            this.btnModificar2.TabIndex = 15;
-            this.btnModificar2.Text = "Modificar";
-            this.btnModificar2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
-            this.btnModificar2.UseVisualStyleBackColor = true;
-            this.btnModificar2.Click += new System.EventHandler(this.btnModificar2_Click);
-            // 
-            // frmnuevacategoria
+            // nuevacategoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(855, 499);
-            this.Controls.Add(this.btnModificar2);
-            this.Controls.Add(this.lblCodigo);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnModificar);
-            this.Controls.Add(this.btnAgregarCategoria);
-            this.Controls.Add(this.dgvCategoria);
+            this.ClientSize = new System.Drawing.Size(605, 270);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.btnRegreso);
             this.Controls.Add(this.txtObervaciones);
@@ -250,11 +175,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "frmnuevacategoria";
+            this.Name = "nuevacategoria";
             this.Text = "nuevacategoria";
-            this.Load += new System.EventHandler(this.frmnuevacategoria_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -272,11 +194,5 @@
         private System.Windows.Forms.TextBox txtObervaciones;
         private System.Windows.Forms.Button btnRegreso;
         private System.Windows.Forms.Button btnGuardar;
-        private System.Windows.Forms.DataGridView dgvCategoria;
-        private System.Windows.Forms.Button btnAgregarCategoria;
-        private System.Windows.Forms.Button btnModificar;
-        private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.Label lblCodigo;
-        private System.Windows.Forms.Button btnModificar2;
     }
 }
--- campeonato/frmnuevocampeonato.cs
+++ campeonato/frmnuevocampeonato.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-using System.Collections;
 
 namespace campeonato
 {
@@ -17,224 +16,98 @@ namespace campeonato
         public frmnuevocampeonato()
         {
             InitializeComponent();
-        }     
+        }
+
+        private MySqlCommand SQL_cmd;
+        private MySqlConnection SQL_conexion = new MySqlConnection();
+        private String S_cadena;
+        
 
         private void botCategoria_Click(object sender, EventArgs e)
         {
-
-            frmnuevacategoria frmncategoria = new frmnuevacategoria();
+            
+            frmCategorias frmncategoria = new frmCategorias();
             frmncategoria.Show();
         }
 
         private void nuevocampeonato_Load(object sender, EventArgs e)
         {
-            // TODO: esta línea de código carga datos en la tabla 'campeonatoDataSet.macategoria' Puede moverla o quitarla según sea necesario.
-            this.macategoriaTableAdapter.Fill(this.campeonatoDataSet.macategoria);
             fncCargagrid();
-        
+            
+
         }
 
-
-        private void limpiar()
+        private void bot1_Click(object sender, EventArgs e)
         {
-            txtNombre.Clear();
-            txtCategoria.Clear();
-            txtDescripcion.Clear();
-            cmbCategoria.Text = "";
-            cmbTipoCompetencia.Text = "";
-        }
 
-        /*
-         * ELIMINACION DE DE DATOS CON CONFIRMACION DE OPERACION
-         * Dennys Choy
-         * 
-         */
-        private void btnEliminar(object sender, EventArgs e)
-        {
             try
             {
-                if (MessageBox.Show("¿Está seguro de Eliminar este registro? Nombre del Empleado: " + txtNombre.Text, "Eliminar Registro", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                {
-                    dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
-                cConectar.cLocal();
-                cConectar.sqlCmd = new MySqlCommand("DELETE FROM macompetencia where ncodcompetencia = " + lblCodigo.Text + ";", cConectar.SqlConexion);
-                cConectar.sqlCmd.ExecuteNonQuery();             
-                cConectar.SqlConexion.Close();
-                fncCargagrid();
-                MessageBox.Show("Registro Eliminado");
-                limpiar();
-                }
-                else
-                {
-                    limpiar();
-                }
-            }
+                S_cadena = "server = localhost; username = root; password = 12345; database = campeonato";
+                SQL_conexion.ConnectionString = S_cadena;
+                SQL_conexion.Open();                                                   
+                SQL_cmd = new MySqlCommand("INSERT INTO macompetencia (vnomcompetencia, vdescripcion, vtipocompetencia,  dfechainicio, dfechafin, MaCATEGORIA_codcategoria) VALUES ('"+ txtNombre.Text +"' , '"+ txtDescripcion.Text +"' , '" +cmbTipoCompetencia.Text+"' , '"+dateInicio.Text+"' , '"+dateFinal.Text+"' , "+ txtCategoria.Text +" ); ", SQL_conexion);
 
-            catch
-            {
-                MessageBox.Show("Problema en BD");
+
+                MySqlDataReader MyReader2;
+                MyReader2 = SQL_cmd.ExecuteReader();
+                MessageBox.Show("Datos Insertados");
+                while (MyReader2.Read())
+                {
+                }
+                SQL_conexion.Close();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
         }
 
-        /*
-        * Carga txtCategoria el codigo de categoria según seleccion de cmbCategoria
-        * Dennys Choy
-        * 
-        */
+        private void bot2_Click(object sender, EventArgs e)
+        {
+            txtDescripcion.Text = cmbCategoria.Text;
+
+        }
+
+
         private void cmbCategoria_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
             try
             {
-                dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
-                cConectar.cLocal();
-                cConectar.sqlCmd = new MySqlCommand("SELECT codcategoria FROM macategoria WHERE vnombrecategoria = '" + cmbCategoria.Text + "';", cConectar.SqlConexion);
-                MySqlDataReader MyReader3;
-                MyReader3 = cConectar.sqlCmd.ExecuteReader();
-                while (MyReader3.Read())
-                {
-                    txtCategoria.Text = MyReader3.GetInt32(0).ToString();
-                    lblCategoria1.Text = MyReader3.GetInt32(0).ToString();
-                }
-                cConectar.SqlConexion.Close();
-            }
+                S_cadena = "server = localhost; username = root; password = 12345; database = campeonato";
+                SQL_conexion.ConnectionString = S_cadena;
+                SQL_conexion.Open();
 
-            catch
+                SQL_cmd = new MySqlCommand("SELECT codcategoria FROM macategoria WHERE vnombrecategoria = '" + cmbCategoria.Text + "';", SQL_conexion);
+
+
+
+                MySqlDataReader MyReader2;
+                MyReader2 = SQL_cmd.ExecuteReader();
+                while (MyReader2.Read())
+                {
+                    txtCategoria.Text = MyReader2.GetInt32(0).ToString();
+                }
+                SQL_conexion.Close();
+            }
+            catch (Exception ex)
             {
-                MessageBox.Show("Problema en BD");
+                MessageBox.Show(ex.Message);
             }
 
         }
 
         private void fncCargagrid()
         {
-            try { 
-            dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
-            cConectar.cLocal();
-            cConectar.sqlData = new MySqlDataAdapter("Select * from macompetencia", cConectar.SqlConexion);
+            string sConexion = "server = localhost; username = root; password = 12345; database = campeonato";
+            MySqlConnection SQL_conexion = new MySqlConnection();
+            SQL_conexion.ConnectionString = sConexion;
+            SQL_conexion.Open();
+            MySqlDataAdapter SQL_da = new MySqlDataAdapter("Select * from macompetencia", SQL_conexion);
             DataTable DT_dat = new DataTable();
-            cConectar.sqlData.Fill(DT_dat);
+            SQL_da.Fill(DT_dat);
             this.dgvCampeonato.DataSource = DT_dat;
-            cConectar.SqlConexion.Close();
-            
-            //Carga datos al combobox según categoría
-         
-            cConectar.cLocal();
-            cConectar.sqlData = new MySqlDataAdapter("select *FROM macategoria", cConectar.SqlConexion);
-            DataTable DT_box = new DataTable();
-            cConectar.sqlData.Fill(DT_box);
-            foreach (DataRow row in DT_box.Rows)
-            {
-                string rowz = row.ItemArray.ToString();
-            }
-            cmbCategoria.DataSource = DT_box;
-            //cboxTipo.DataSource = DT_box;
-            //cboxTipo.DisplayMember = "ncodpuesto";
-            cmbCategoria.DisplayMember = "vnombrecategoria";
-            cConectar.SqlConexion.Close();
-
-
-
-            }
-            catch
-            {
-                MessageBox.Show("Problema en BD");
-            }
-
-        }
-
-        /*
-        *Carga datos según seleccion del grid
-        * Dennys Choy
-        * 
-        */
-
-        private void dgvCampeonato_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-             try
-            {
-                lblCodigo.Text = dgvCampeonato[0, dgvCampeonato.CurrentCell.RowIndex].Value.ToString();
-                txtNombre.Text = dgvCampeonato[1, dgvCampeonato.CurrentCell.RowIndex].Value.ToString(); 
-                //cmbCategoria.Text = dgvCampeonato[6, dgvCampeonato.CurrentCell.RowIndex].Value.ToString();
-                cmbTipoCompetencia.Text = dgvCampeonato[3, dgvCampeonato.CurrentCell.RowIndex].Value.ToString();
-                dateInicio.Text = dgvCampeonato[4, dgvCampeonato.CurrentCell.RowIndex].Value.ToString();
-                dateFinal.Text = dgvCampeonato[5, dgvCampeonato.CurrentCell.RowIndex].Value.ToString();
-                txtDescripcion.Text = dgvCampeonato[2, dgvCampeonato.CurrentCell.RowIndex].Value.ToString();
-            }
-            catch
-            {
-                MessageBox.Show("Problema al cargar BD");
-            }
-        }
-        /*
-        * "UDPDATE" DE DE DATOS CON CONFIRMACION DE OPERACION
-        * Dennys Choy
-        * 
-        */
-        private void btnModificar(object sender, EventArgs e)
-        {
-            try
-            {
-                if (MessageBox.Show("¿Está seguro Modificar este registro? Nombre del Empleado: " + txtNombre.Text, "Modificar Registro", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                    {
-                dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
-                cConectar.cLocal();
-                cConectar.sqlCmd = new MySqlCommand("UPDATE macompetencia SET vnomcompetencia = '"+ txtNombre.Text+ "', vdescripcion = '" + txtDescripcion.Text + "' , vtipocompetencia = '"+ cmbTipoCompetencia.Text +"' ,  dfechainicio = '"+ dateInicio.Text +"', dfechafin = '"+ dateFinal.Text + "' , MaCATEGORIA_codcategoria = "+ txtCategoria.Text +" WHERE ncodcompetencia = "+lblCodigo.Text+";" , cConectar.SqlConexion);
-                cConectar.sqlCmd.ExecuteNonQuery();
-                cConectar.SqlConexion.Close();
-                fncCargagrid();
-                MessageBox.Show("Registro Modificado");
-                limpiar();
-                    }
-                else
-                {
-                    limpiar();
-                }
-            }
-
-            catch
-            {
-                MessageBox.Show("Problema en BD");
-            }
-
-            
-               
-        }
-        //INSERTAR DATOS A LA TABLA CAMPEONATO
-        private void btnGuarda_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
-                cConectar.cLocal();
-                cConectar.sqlCmd = new MySqlCommand("INSERT INTO macompetencia (vnomcompetencia, vdescripcion, vtipocompetencia,  dfechainicio, dfechafin, MaCATEGORIA_codcategoria) VALUES ('" + txtNombre.Text + "' , '" + txtDescripcion.Text + "' , '" + cmbTipoCompetencia.Text + "' , '" + dateInicio.Text + "' , '" + dateFinal.Text + "' , " + lblCategoria1.Text + " ); ", cConectar.SqlConexion);
-                MySqlDataReader MyReader2;
-                MyReader2 = cConectar.sqlCmd.ExecuteReader();
-                MessageBox.Show("Datos Insertados");
-                cConectar.SqlConexion.Close();
-                fncCargagrid();
-            }
-            catch
-            {
-                MessageBox.Show("Error en BD");
-            } 
-        }
-
-        private void txtNombre_TextChanged(object sender, EventArgs e)
-        {
-            dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
-            cConectar.cLocal();
-            cConectar.sqlData = new MySqlDataAdapter("Select * from macompetencia where ndpijugador like ('" + txtNombre.Text + "%') ", cConectar.SqlConexion);
-
-            DataTable DT_dat = new DataTable();
-            cConectar.sqlData.Fill(DT_dat);
-            this.dgvCampeonato.DataSource = DT_dat;
-            cConectar.SqlConexion.Close();
-        }
-
-        private void cmbTipoCompetencia_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            SQL_conexion.Close();
         }
     }
 }
