deb12701765dbaaf484b702411880cc57e590f53
daviidbarrios <david_barrios@hotmail.com>
2015-08-31 23:35:17 -0600

    Reporte Jugador
    Aqui esta el reporte de los jugadores inscritos

---
+++ campeonato/frmreportejugador.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace campeonato
+{
+    partial class frmreportejugador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.majugadorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.campeonatoDataSet1 = new campeonato.campeonatoDataSet1();
+            this.majugadorTableAdapter = new campeonato.campeonatoDataSet1TableAdapters.majugadorTableAdapter();
+            this.dgvReporteJugador = new System.Windows.Forms.DataGridView();
+            this.cbmEquipo = new System.Windows.Forms.ComboBox();
+            this.lblCodigoEquipo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.majugadorBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.campeonatoDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporteJugador)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // majugadorBindingSource
+            // 
+            this.majugadorBindingSource.DataMember = "majugador";
+            this.majugadorBindingSource.DataSource = this.campeonatoDataSet1;
+            // 
+            // campeonatoDataSet1
+            // 
+            this.campeonatoDataSet1.DataSetName = "campeonatoDataSet1";
+            this.campeonatoDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // majugadorTableAdapter
+            // 
+            this.majugadorTableAdapter.ClearBeforeFill = true;
+            // 
+            // dgvReporteJugador
+            // 
+            this.dgvReporteJugador.AllowUserToAddRows = false;
+            this.dgvReporteJugador.AllowUserToDeleteRows = false;
+            this.dgvReporteJugador.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvReporteJugador.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvReporteJugador.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporteJugador.Location = new System.Drawing.Point(45, 127);
+            this.dgvReporteJugador.Name = "dgvReporteJugador";
+            this.dgvReporteJugador.ReadOnly = true;
+            this.dgvReporteJugador.Size = new System.Drawing.Size(625, 293);
+            this.dgvReporteJugador.TabIndex = 0;
+            this.dgvReporteJugador.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvReporteJugador_CellContentClick);
+            // 
+            // cbmEquipo
+            // 
+            this.cbmEquipo.FormattingEnabled = true;
+            this.cbmEquipo.Location = new System.Drawing.Point(239, 80);
+            this.cbmEquipo.Name = "cbmEquipo";
+            this.cbmEquipo.Size = new System.Drawing.Size(210, 21);
+            this.cbmEquipo.TabIndex = 1;
+            this.cbmEquipo.SelectedIndexChanged += new System.EventHandler(this.cbmEquipo_SelectedIndexChanged);
+            // 
+            // lblCodigoEquipo
+            // 
+            this.lblCodigoEquipo.AutoSize = true;
+            this.lblCodigoEquipo.Location = new System.Drawing.Point(468, 83);
+            this.lblCodigoEquipo.Name = "lblCodigoEquipo";
+            this.lblCodigoEquipo.Size = new System.Drawing.Size(10, 13);
+            this.lblCodigoEquipo.TabIndex = 2;
+            this.lblCodigoEquipo.Text = ".";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Elija el nombre del equipo:";
+            // 
+            // frmreportejugador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 446);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCodigoEquipo);
+            this.Controls.Add(this.cbmEquipo);
+            this.Controls.Add(this.dgvReporteJugador);
+            this.Name = "frmreportejugador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmreportejugador";
+            this.Load += new System.EventHandler(this.frmreportejugador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.majugadorBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.campeonatoDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporteJugador)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource majugadorBindingSource;
+        private campeonatoDataSet1 campeonatoDataSet1;
+        private campeonatoDataSet1TableAdapters.majugadorTableAdapter majugadorTableAdapter;
+        private System.Windows.Forms.DataGridView dgvReporteJugador;
+        private System.Windows.Forms.ComboBox cbmEquipo;
+        private System.Windows.Forms.Label lblCodigoEquipo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ campeonato/frmreportejugador.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Security.Permissions;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Principal;
+namespace campeonato
+{
+    public partial class frmreportejugador : Form
+    {
+        public frmreportejugador()
+        {
+            InitializeComponent();
+        }
+
+        private void frmreportejugador_Load(object sender, EventArgs e)
+        {
+
+            fncCargacmb1();
+        }
+
+        private void fncCargacmb1()
+        {
+
+            try
+            {
+                dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
+                cConectar.cLocal();
+                cConectar.sqlData = new MySqlDataAdapter("Select vnombreequipo from maequipo", cConectar.SqlConexion);
+                DataTable DT_box = new DataTable();
+                cConectar.sqlData.Fill(DT_box);
+                foreach (DataRow row in DT_box.Rows)
+                {
+                    string rowz = row.ItemArray.ToString();
+                }
+                cbmEquipo.DataSource = DT_box;
+                cbmEquipo.DisplayMember = "vnombreequipo";
+
+                cConectar.SqlConexion.Close();
+            }
+
+            catch
+            {
+                MessageBox.Show("Problema en BD, Competencia");
+            }
+
+        }
+
+        private void cbmEquipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
+                cConectar.cLocal();
+                cConectar.sqlCmd = new MySqlCommand("Select ncodequipo from maequipo where vnombreequipo = '" + cbmEquipo.Text + "';", cConectar.SqlConexion);
+                MySqlDataReader MyReader3;
+                MyReader3 = cConectar.sqlCmd.ExecuteReader();
+
+                while (MyReader3.Read())
+                {
+                    lblCodigoEquipo.Text = MyReader3.GetInt32(0).ToString();
+                    fncCargagrid();
+                }
+                cConectar.SqlConexion.Close();
+            }
+
+            catch
+            {
+                MessageBox.Show("Problema en BD");
+            }
+
+        }
+
+        private void fncCargagrid()
+        {
+
+
+            try
+            {
+                dll_conexion.Conexion cConectar = new dll_conexion.Conexion();
+                cConectar.cLocal();
+                //cConectar.sqlData = new MySqlDataAdapter("SELECT *FROM trequipoencompetencia WHERE MaCOMPETENCIA_ncodcompetencia1 = "+ lblCodigoCompetencia.Text +";", cConectar.SqlConexion);
+                cConectar.sqlData = new MySqlDataAdapter("select ndpijugador, vnombrejugador, vapejugador, nfechanacimiento, vsexojugador, vdireccionjugador, vtelefonojugador from majugador where maequipo_ncodequipo = '" +lblCodigoEquipo.Text + "';", cConectar.SqlConexion);
+                DataTable DT_dat = new DataTable();
+                cConectar.sqlData.Fill(DT_dat);
+
+                this.dgvReporteJugador.DataSource = DT_dat;
+
+
+                cConectar.SqlConexion.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Problema en BD, Equipo en competencia GRID");
+            }
+
+        }
+
+        private void dgvReporteJugador_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
